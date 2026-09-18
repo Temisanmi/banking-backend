c96d63a362372e34d64e869a07def5cd714cf5de
Temisanmi <temiifesanmi@gmail.com>
2026-09-18 20:35:09 +0100

    Created core DTO's for transaction

---
+++ src/main/java/com/temi/banking_backend/dto/transaction/DepositRequest.java
@@ -0,0 +1,21 @@
+package com.temi.banking_backend.dto.transaction;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class DepositRequest {
+    @NotBlank(message = "Account ID is required")
+    private String accountId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must e greater than zero")
+    private BigDecimal amount;
+
+    private String description;
+}
---
+++ src/main/java/com/temi/banking_backend/dto/transaction/TransactionResponse.java
@@ -0,0 +1,41 @@
+package com.temi.banking_backend.dto.transaction;
+
+import com.temi.banking_backend.entity.Transaction;
+import com.temi.banking_backend.entity.enums.TransactionStatus;
+import com.temi.banking_backend.entity.enums.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class TransactionResponse {
+    private String id;
+    private TransactionType type;
+    private BigDecimal amount;
+    private String fromAccountNumber;
+    private String toAccountNumber;
+    private String performedByName;
+    private TransactionStatus status;
+    private String description;
+    private LocalDateTime initiatedAt;
+    private LocalDateTime completedAt;
+
+    public static TransactionResponse fromEntity(Transaction transaction){
+        return new TransactionResponse(
+                transaction.getId(),
+                transaction.getType(),
+                transaction.getAmount(),
+                transaction.getFromAccount() != null ? transaction.getFromAccount().getAccountNumber() : null,
+                transaction.getToAccount() != null ? transaction.getToAccount().getAccountNumber() : null,
+                transaction.getPerformedBy().getFullName(),
+                transaction.getStatus(),
+                transaction.getDescription(),
+                transaction.getInitiatedAt(),
+                transaction.getCompletedAt()
+        );
+    }
+}
